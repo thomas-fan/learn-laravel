d81b0fa1cf7610ada8d009e48cb6d5effb78b88b
thomas-fan <youxiboy007@vip.qq.com>
2019-09-16 11:37:56 +0800

    登陆后导航逻辑

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+$('.dropdown').on('click', function (e) {
+  e.stopPropagation()
+  $('.dropdown-menu').css({'display': 'block'})
+})
+$('body').on('click', function () {
+  $('.dropdown-menu').css({'display': 'none'})
+})
