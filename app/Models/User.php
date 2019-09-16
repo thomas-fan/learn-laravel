6c6f60298e37bafbf15e238a95a25efb607e132d
thomas-fan <youxiboy007@vip.qq.com>
2019-09-16 18:17:29 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
 }
